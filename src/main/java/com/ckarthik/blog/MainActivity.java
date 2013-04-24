6aeec8f21b0316ca655e5a84126590af9fd8af4f
Karthik Chandraraj <ckarthik17@gmail.com>
2013-04-24 23:04:42 +0530

    Location updates test

--- src/main/java/com/ckarthik/blog/MainActivity.java
+++ src/main/java/com/ckarthik/blog/MainActivity.java
@@ -1,18 +1,53 @@
 package com.ckarthik.blog;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends Activity {
+import java.util.List;
+
+public class MainActivity extends Activity implements LocationListener {
+    private Location latestLocation;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_layout);
+        initLocationListener();
+    }
+
+    private void initLocationListener() {
+        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);;
+        List<String> allProviders = locationManager.getAllProviders();
+        for (String provider : allProviders) {
+            locationManager.requestLocationUpdates(provider, 0, 0, this);
+        }
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        latestLocation = location;
+    }
+
+    public Location latestLocation() {
+        return latestLocation;
     }
 
     public void onButtonClick(View v) {
         startActivity(new Intent(this, SecondActivity.class));
     }
+
+    @Override
+    public void onStatusChanged(String s, int i, Bundle bundle) {}
+
+    @Override
+    public void onProviderDisabled(String s) {}
+
+    @Override
+    public void onProviderEnabled(String s) {}
 }
--- src/test/java/com/ckarthik/blog/MainActivityTest.java
+++ src/test/java/com/ckarthik/blog/MainActivityTest.java
@@ -1,14 +1,21 @@
 package com.ckarthik.blog;
 
+import android.content.Context;
 import android.content.Intent;
+import android.location.Location;
+import android.location.LocationManager;
 import android.widget.Button;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.shadows.ShadowActivity;
+import org.robolectric.shadows.ShadowLocationManager;
 
+import static android.location.LocationManager.NETWORK_PROVIDER;
 import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
 import static org.robolectric.Robolectric.shadowOf;
 
@@ -22,6 +29,26 @@ public class MainActivityTest {
         mainActivity = new MainActivity();
         mainActivity.onCreate(null);
         button = (Button) mainActivity.findViewById(R.id.open_button);
+    }
+
+    @Test
+    public void shouldReturnTheLatestLocation() {
+        LocationManager locationManager = (LocationManager) Robolectric.application.getSystemService(Context.LOCATION_SERVICE);
+        ShadowLocationManager shadowLocationManager = shadowOf(locationManager);
+        Location expectedLocation = location(NETWORK_PROVIDER, 12.0, 20.0);
+
+        shadowLocationManager.simulateLocation(expectedLocation);
+        Location actualLocation = mainActivity.latestLocation();
+
+        assertEquals(expectedLocation, actualLocation);
+    }
+
+    private Location location(String provider, double latitude, double longitude) {
+        Location location = new Location(provider);
+        location.setLatitude(latitude);
+        location.setLongitude(longitude);
+        location.setTime(System.currentTimeMillis());
+        return location;
     }
 
     @Test
